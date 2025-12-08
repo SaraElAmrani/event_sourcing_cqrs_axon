153b2a9ab89381b0846f023a6557de3c786aa5bb
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-12-08 10:37:07 +0100

    Initial commit

---
+++ src/test/java/ma/elamrani/event_sourcing_cqrs_axon/EventSourcingCqrsAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.event_sourcing_cqrs_axon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingCqrsAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
