2e09e2e390ccd698bc925218972bd03d8529451d
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-12-28 18:43:16 +0100

    feat(api): add account command REST controller

--- src/main/java/ma/elamrani/event_sourcing_cqrs_axon/commands/controllers/AccountCommandController.java
+++ src/main/java/ma/elamrani/event_sourcing_cqrs_axon/commands/controllers/AccountCommandController.java
@@ -1,22 +1,33 @@
 package ma.elamrani.event_sourcing_cqrs_axon.commands.controllers;
 
 
+import ma.elamrani.event_sourcing_cqrs_axon.commands.aggregates.AccountAggregate;
 import ma.elamrani.event_sourcing_cqrs_axon.commands.commands.AddAccountCommand;
+import ma.elamrani.event_sourcing_cqrs_axon.commands.commands.CreditAccountCommand;
+import ma.elamrani.event_sourcing_cqrs_axon.commands.commands.DebitAccountCommand;
+import ma.elamrani.event_sourcing_cqrs_axon.commands.commands.UpdateAccountStatusCommand;
 import ma.elamrani.event_sourcing_cqrs_axon.commands.dtos.AddNewAccountRequestDTO;
+import ma.elamrani.event_sourcing_cqrs_axon.commands.dtos.CreditAccountRequestDTO;
+import ma.elamrani.event_sourcing_cqrs_axon.commands.dtos.DebitAccountRequestDTO;
+import ma.elamrani.event_sourcing_cqrs_axon.commands.dtos.UpdateAccountStatusRequestDTO;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("commands/accounts")
 public class AccountCommandController {
 
     private CommandGateway commandGateway;
+    private EventStore eventStore;
 
-    public AccountCommandController(CommandGateway commandGateway) {
+    public AccountCommandController(CommandGateway commandGateway, EventStore eventStore) {
         this.commandGateway = commandGateway;
+        this.eventStore = eventStore;
     }
 
     @PostMapping("/add")
@@ -29,9 +40,48 @@ public class AccountCommandController {
         return response;
     }
 
+    @PostMapping("/credit")
+    public CompletableFuture<String> creditAccount(@RequestBody CreditAccountRequestDTO request){
+        CompletableFuture<String> response = commandGateway.send(new CreditAccountCommand(
+                request.accountId(),
+                request.amount(),
+                request.currency()
+        ));
+        return response;
+    }
+
+    @PostMapping("/debit")
+    public CompletableFuture<String> debitAccount(@RequestBody DebitAccountRequestDTO request){
+        CompletableFuture<String> response = commandGateway.send(new DebitAccountCommand(
+                request.accountId(),
+                request.amount(),
+                request.currency()
+        ));
+        return response;
+    }
+
+    @PutMapping("/updateStatus")
+    public CompletableFuture<String> updateStatus(@RequestBody UpdateAccountStatusRequestDTO request){
+        CompletableFuture<String> response = this.commandGateway.send(new UpdateAccountStatusCommand(
+                request.accountId(),
+                request.accountStatus()
+        ));
+        return response;
+    }
+
     @ExceptionHandler(Exception.class)
     public String exceptionHandler(Exception  exception){
         return exception.getMessage();
     }
+
+    @GetMapping("/events/{accountId}")
+    public Stream eventStore(@PathVariable String accountId){
+        return eventStore.readEvents(accountId).asStream();
+    }
+
+    /*@GetMapping("/accountAggregate")
+    public AccountAggregate accountAggregate(){
+        return accountAggregate;
+    } */
 
 }
